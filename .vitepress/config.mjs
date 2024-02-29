d9841ad0f0b9ebd60452a7972e91a6b2ca4f1384
Betim Hodza <106614572+Betim-Hodza@users.noreply.github.com>
2024-02-29 18:38:08 +0000

    feat: added vitepress template

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "CSEC-CTF-NOTES",
-  description: "CTF Repo for CSEC",
+  title: "CSEC CTF",
+  description: "A VitePress Site",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
