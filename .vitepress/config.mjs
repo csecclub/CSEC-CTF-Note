500ae578de25b81b151d49075e9e6105876aa6d4
betim-hodza <bhodza02@gmail.com>
2024-03-01 19:16:27 -0600

    returned to default base

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/CTF Notes',
+  base: '/',
   title: "CSEC CTF Notes",
   description: "repo for all your ctf needs",
   
