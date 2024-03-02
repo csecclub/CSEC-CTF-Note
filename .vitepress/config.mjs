3787a57f6b01256b2de1c8bf49bbe06ed10b2df3
betim-hodza <bhodza02@gmail.com>
2024-03-01 18:47:53 -0600

    changed base path

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/',
+  base: '/CTF notes',
   title: "CSEC CTF Notes",
   description: "repo for all your ctf needs",
   
